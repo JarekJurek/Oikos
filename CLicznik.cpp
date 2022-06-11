28c83cee84c25fbcf16cb75a48738ce52b29c763
Grzegorz <grzes.pawlak@gmail.com>
2022-06-11 18:34:18 +0200

    Taryfa refactored. probably finished

--- CLicznik.cpp
+++ CLicznik.cpp
@@ -5,3 +5,26 @@
 #include "CLicznik.h"
 
 
+void CLicznik::podajDaneLicznika(string p) {
+    numerLicznika = p;
+}
+
+string CLicznik::outJednostka() {
+    return jednostka;
+}
+
+string CLicznik::outNumerLicznika() {
+    return numerLicznika;
+}
+
+void CLicznik::zmienJednostke(string s) {
+    jednostka = s;
+}
+
+void CLicznik::zmienTyp(string s) {
+    typ = s;
+}
+
+string CLicznik::outTyp() {
+    return typ;
+}
