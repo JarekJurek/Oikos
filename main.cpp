2144260050b8b67dc6dc74b1f56089027df623e1
Grzegorz <grzes.pawlak@gmail.com>
2022-06-11 13:05:42 +0200

    (de)serializer and CListaMieszkan as params

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@
 #include <deque>
 
 int main() {
-    CListaMieszkan l;
-    CInterfejs::mainManu(&l);
+    CInterfejs start;
+    start.mainManu();
+
 //    deque <int> test;
 //    for (int i=1; i<=10; i++) test.push_back(i);
 //    for (int i=1; i<=10; i++) cout << test [i];
 //    test.erase(test.begin()+5);
 //    for (int i=0; i<test.size(); i++) cout << test [i];
-
 
 }
 
